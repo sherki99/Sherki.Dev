eaf4b34cfae18fc83d5035157194eb8cd940f50c
Cherki Meziane <101719089+sherki99@users.noreply.github.com>
2025-03-28 05:09:39 +0100

    ee

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -17,7 +17,7 @@ const Photo = () => {
       >
         <Image
           src="/assets/photo.png"
-          alt="Luiso's profile photo"
+          alt="Sherki's profile photo"
           priority
           quality={100}
           sizes="(max-width: 600px) 100vw, (max-width: 1200px) 50vw, 400px"
